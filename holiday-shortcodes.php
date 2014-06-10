808599b13ef1a1dc791f9e8fedbf464d7a1f4024
Kyle <kyle@realbigmarketing.com>
2014-06-09 23:27:53 -0400

    Employ new extending method
    Now use the add_usl_shortcode method

--- holiday-shortcodes.php
+++ holiday-shortcodes.php
@@ -6,12 +6,6 @@ Version: 1.0
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
-//Add our new category
-function usl_add_holiday_cat($usl_cats) {
-$usl_cats[]='Holiday';
-return $usl_cats;
-}
-add_filter('usl_extend_cats', 'usl_add_holiday_cat');
 
 /*-------------------------------
 Days until Christmas
@@ -25,18 +19,15 @@ function usl_days_until_christmas() {
 	$days =(int) ($difference/86400);
 	return $days;
 }
-add_shortcode( 'usl_days_until_christmas', 'usl_days_until_christmas' );
-
-function usl_add_duc($usl_codes) {
-$usl_codes[] = array(
-		'Title'=>'Days until Christmas',
-		'Code'=>'usl_days_until_christmas',
-		'Description'=>'Displays the number of days remaining until Christmas day.',
-		'Category'=>'Holiday'
-		);
-return $usl_codes;
+function add_duc_please() {
+add_usl_shortcode( 'usl_days_until_christmas',
+	'usl_days_until_christmas',
+	'Days Until Christmas',
+	'Displays the number of days remaining until Christmas day.',
+	'Holiday' );
 }
-add_filter('usl_extend_codes', 'usl_add_duc');
+add_action('plugins_loaded', 'add_duc_please');
+
 /*-------------------------------
 Days until Halloween
 -------------------------------*/
@@ -51,16 +42,14 @@ function usl_days_until_halloween() {
 }
 add_shortcode( 'usl_days_until_halloween', 'usl_days_until_halloween' );
 
-function usl_add_duh($usl_codes) {
-$usl_codes[] = array(
-		'Title'=>'Days until Halloween',
-		'Code'=>'usl_days_until_halloween',
-		'Description'=>'Displays the number of days remaining until Halloween.',
-		'Category'=>'Holiday'
-		);
-return $usl_codes;
+function usl_add_duh() {
+add_usl_shortcode( 'usl_days_until_halloween',
+	'usl_days_until_halloween',
+	'Days Until Halloween',
+	'Displays the number of days remaining until Halloween.',
+	'Holiday' );
 }
-add_filter('usl_extend_codes', 'usl_add_duh');
+add_action('plugins_loaded', 'usl_add_duh');
 /*-------------------------------
 Random Christmas lyrics
 -------------------------------*/
@@ -104,4 +93,3 @@ $usl_codes[] = array(
 return $usl_codes;
 }
 add_filter('usl_extend_codes', 'usl_add_du');
-?>
